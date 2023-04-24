c3ad218920181f392becc3e7f39939b9907ea3d7
Kamal <kamalios@mail.ru>
2023-04-24 20:16:50 +0300

    Initial Commit

---
+++ ColorMixerSwiftUI/ColorMixerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerSwiftUIApp.swift
+//  ColorMixerSwiftUI
+//
+//  Created by Камаль Атавалиев on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
